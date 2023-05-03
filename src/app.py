5d0915eb204ee3fef1c5705a9494f581a0c58d28
DanielxDante <danielonline170600@outlook.com>
2023-05-03 15:13:30 +0800

    transferred existing files to new version

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def home():
     return '<h1>Hello, World!</h1>'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/portal1') 
+def portal1():
+    return render_template('API Exchange Developer Portal.html')
+
+@app.route('/portal2')
+def portal2():
+    return render_template('API Exchange Developer Portal2.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
