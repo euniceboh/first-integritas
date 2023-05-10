d7c4e277259be68dfc8f55bb99afb2295eab3f8c
DanielxDante <danielonline170600@outlook.com>
2023-05-10 10:39:49 +0800

    resolving double post; resetting azure pipeline yaml

--- test.js
+++
@@ -1,9 +0,0 @@
-function splitSubtier(subtier) {
-    var probs = [1, 0];
-    var lasts = [0];
-    for (let i = 1; i < len(subtier); i++) {
-      console.log(subtier[i]);
-    }
-  }
-
-  splitSubtier("updateMemberCreditStatusForPayNow");
