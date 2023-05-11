b4a275db005e80b2d7e109c60a40945f1b902deb
DanielxDante <danielonline170600@outlook.com>
2023-05-11 10:37:27 +0800

    added makefile for development, changed to test_functional to accommodate test_unit

---
+++ Makefile
@@ -0,0 +1,29 @@
+.ONESHELL:
+
+.DEFAULT_GOAL := run
+
+PYTHON = ./venv/Scripts/python
+PIP = ./venv/Scripts/pip
+
+venv/Scripts/activate: requirements.txt
+	python -m venv venv
+	. ./venv/Scripts/activate
+	$(PIP) install -r requirements.txt
+
+venv: venv/Scripts/activate
+	. ./venv/Scripts/activate
+
+run: venv test
+	$(PYTHON) src/app.py
+
+test:
+	. ./venv/Scripts/activate; pytest
+
+clean:
+	cd src; rm -rf __pycache__
+	cd ..; rm -rf venv
+
+.PHONY: run clean
+
+
+
--- src/tests/test_functional.py
+++ src/tests/test_functional.py
@@ -1,3 +1,5 @@
+# White Box Functional Testing
+
 from src import app as flask_app
 import json
 import pytest
@@ -6,7 +8,6 @@ import pytest
 def app():
     return flask_app.app
 
-# Will do most of the route (only 2) testing as Unit Testing 
 def test_catchall_route(app):
     client = app.test_client()
     url = '/any/other/route'
@@ -28,20 +29,10 @@ def test_swaggeruipreview_route(app):
 def test_openapi():
     assert flask_app.checkOpenapi("3.0.0") == True
 
+def test_openapi_unsuccessful():
+    assert flask_app.checkOpenapi("") == False
+    assert flask_app.checkOpenapi(None) == False
+
 def test_title():
     assert flask_app.checkTitle("Update Crediting Status of 55 WDL Application PayNow") == True
 
-# def test_post_route__failure__bad_request():
-#     app = Flask(__name__)
-#     configure_routes(app)
-#     client = app.test_client()
-#     url = '/post/test'
-
-#     mock_request_headers = {
-#         'authorization-sha256': '123'
-#     }
-
-#     mock_request_data = {}
-
-#     response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_headers)
-#     assert response.status_code == 400
