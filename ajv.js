6cfbd7b5c54ececc2c2987f2a4f9761c13b7b464
DanielxDante <danielonline170600@outlook.com>
2023-06-07 17:06:25 +0800

    7/6/2023 managed to complete the schema by following swaggerui. not sure if it performs the same way because i converted the swaggerui schema from json draft 4 to 7 (yes surprisingly there are set keywords on the web) which changes mainly the parts on switch (which is a custom keyword by swaggerui) but later on adopted into if else then on json draft 7. nevertheless, it is done and im done with this... now for the custom validation rules and spelling checking, which i assume i will need to create custom keywords for both of those (idk how halp) then we can start on the gui (not sure if we have the time to follow editor-next, which looks amazing)

--- ajv.js
+++ ajv.js
@@ -1,34 +1,63 @@
-import Ajv from "ajv"
-import yaml from "js-yaml"
-import fs from "fs"
+const Ajv = require("ajv");
+const AjvErrors = require("ajv-errors");
+const AjvKeywords = require("ajv-keywords");
+const addFormats = require("ajv-formats")
+const yaml = require("js-yaml");
+const fs = require("fs");
+
+const schema = yaml.load(fs.readFileSync('oas3.0_schema.yaml', 'utf-8'))
+const data = yaml.load(fs.readFileSync('template1.yaml', 'utf-8'))
 
 const ajv = new Ajv({
-  schemaId: "auto",
+  schemaId: "id",
   allErrors: true,
-  jsonPointers: true,
-}) 
+})
+addFormats(ajv, ["uri-reference", "email", "regex", "uri"])
+AjvErrors(ajv) // for the errorMessage keyword
 
-// const schema = yaml.load(fs.readFileSync('oas3.0_schema.yaml', 'utf-8'))
-// console.log(schema)
-
-const schema = {
-  $schema: 'http://json-schema.org/draft-04/schema#',
-  type: 'object',
-  properties: {
-    openapi: { type: 'string' },
-    info: { type: 'string' },
-    paths: { type: 'integer' }
+// TODO: Spelling Checking Feature
+// Consider using a keyword on each value for spelling checking
+const spellingKeyword = {
+  keyword: 'spelling',
+  validate: (schema, data) => {
+    if (typeof data === 'string') {
+      const isSpelledCorrectly = spellchecker.isMisspelled(data);
+      return !isSpelledCorrectly;
+    }
+    return true;
   },
-  required: ['openapi', 'info', 'paths']
-}
+  errors: true // Enable error messages for the keyword
+};
+
+// TODO: Custom Validation Rules
+// Consider using a keyword on keys
 
 const validate = ajv.compile(schema)
 
-const data = {
-  openapi: "test",
-  info: "test",
-  paths: 0
+function getLineNumberFromPath(dataJson, pathArray) {
+  if (pathArray.length == 0) {
+    return 0
+  }
+  // TODO: Finish getting line number from path array. Can call our python function if
+  // its too hard to get from JS
 }
 
-const valid = validate(data)
-if (!valid) console.log(validate.errors)
+if (!validate(data)) {
+  for (let i = 0; i < validate.errors.length; i++) {
+    if (["if", "then", "else"].includes(validate.errors[i]["keyword"])) {
+      continue;
+    }
+    var path = validate.errors[i]["instancePath"]
+    var pathArray = path.split("/").slice(1,)
+    // change back "/" that are converted from ajv
+    for (let j = 0; j < pathArray.length; j++) {
+      if (pathArray[j]) {
+        pathArray[j] = pathArray[j].replace(/~1/g, "/")
+      }
+    }
+    var lineNumber = getLineNumberFromPath(data, pathArray)
+    console.log(lineNumber)
+    console.log(pathArray)
+    console.log(validate.errors[i])
+  }
+}
