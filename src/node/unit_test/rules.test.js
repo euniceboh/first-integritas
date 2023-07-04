e94557e02b1e0f9a977609f0b951439b74d18ed1
DanielxDante <danielonline170600@outlook.com>
2023-07-04 13:42:12 +0800

    added fail test cases

--- src/node/unit_test/rules.test.js
+++ src/node/unit_test/rules.test.js
@@ -5,7 +5,7 @@ const fileContent = fs.readFileSync('./unit_test/uris.txt', 'utf8');
 let paths = fileContent.split('\r\n');
 
 // for (const path of paths) {
-    const path = "/medicalInsurance/mediShieldLife/v1/getMemberDependants"
+    let path = "/medicalInsurance/mediShieldLife/v1/getMemberDependants"
     test(`checkPathCharacters -- ${path}`, () => {
         expect(rules.checkPathCharacters(path)).toBe(true)
     })
@@ -32,6 +32,45 @@ let paths = fileContent.split('\r\n');
     });
 // }
 
+let path_fail_checkPathCharacters = "/medicalInsurance/medi-ShieldLife/v1/getMemberDependants"
+test(`Fail checkPathCharacters --- ${path_fail_checkPathCharacters}`, () => {
+    expect(rules.checkPathCharacters(path_fail_checkPathCharacters)).toBe(false)
+})
+
+path_fail_checkPathLength = "/medicalInsurance/mediShieldLife/v1/getMemberDependants"
+test(`Fail checkPathLength --- ${path_fail_checkPathLength}`, () => {
+    expect(rules.checkPathLength(path_fail_checkPathLength)).toBe(false)
+})
+
+path_fail_checkAPIProduct = "/medicalInsurance/mediShieldLife/v1/getMemberDependants"
+test(`Fail checkAPIProduct --- ${path_fail_checkAPIProduct}`, () => {
+    expect(rules.checkAPIProduct(path_fail_checkAPIProduct)).toBe(false)
+})
+
+path_fail_checkPathVersion = "/medicalInsurance/mediShieldLife/v1/getMemberDependants"
+test(`Fail checkPathVersion --- ${path_fail_checkPathVersion}`, () => {
+    expect(rules.checkPathVersion(path_fail_checkPathVersion)).toBe(false)
+})
+
+path_fail_checkMatchingVersion = "/medicalInsurance/mediShieldLife/v1/getMemberDependants"
+test(`Fail checkMatchingVersion --- ${path_fail_checkMatchingVersion}`, () => {
+    expect(rules.checkMatchingVersion(path_fail_checkMatchingVersion)).toBe(false)
+})
+
+path_fail_checkCamelCasing = "/medicalInsurance/mediShieldLife/v1/getMemberDependants"
+test(`Fail checkCamelCasing --- ${path_fail_checkCamelCasing }`, () => {
+    expect(rules.checkCamelCasing(path_fail_checkCamelCasing )).toBe(false)
+})
+
+path_fail_checkPathSpelling = "/medicalInsurance/mediShieldLife/v1/getMemberDependants"
+test(`Fail checkPathSpelling --- ${path_fail_checkPathSpelling}`, () => {
+    expect(rules.checkPathSpelling(path_fail_checkPathSpelling)).toBe(false)
+})
+
+path_fail_checkPathVerb = "/medicalInsurance/mediShieldLife/v1/getMemberDependants"
+test(`Fail checkPathVerb --- ${path_fail_checkPathVerb}`, () => {
+    expect(rules.checkVerb(path_fail_checkPathVerb)).toBe(false)
+})
 
 
 
