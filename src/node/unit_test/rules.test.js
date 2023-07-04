e3f6b8dfea536e445efece08c60e060673babb02
DanielxDante <danielonline170600@outlook.com>
2023-07-04 14:05:07 +0800

    again...

--- src/node/unit_test/rules.test.js
+++ src/node/unit_test/rules.test.js
@@ -57,12 +57,12 @@ test(`Fail checkMatchingVersion --- ${path_fail_checkMatchingVersion}`, () => {
     expect(rules.checkMatchingVersion(path_fail_checkMatchingVersion)).toBe(false)
 })
 
-path_fail_checkCamelCasing = "/medicalInsurance/mediShieldlife/v1/getMemberDependants"
+path_fail_checkCamelCasing = "/medicalInsurance/mediShieldlife/v1/getMemberDepenants"
 test(`Fail checkCamelCasing --- ${path_fail_checkCamelCasing }`, () => {
     expect(rules.checkCamelCasing(path_fail_checkCamelCasing )).toBe(false)
 })
 
-path_fail_checkPathSpelling = "/medicalInsurance/mediShieldLfe/v1/getMemberDependants"
+path_fail_checkPathSpelling = "/medicalInsurance/mediShieldLfe/v1/getMemberDepeants"
 test(`Fail checkPathSpelling --- ${path_fail_checkPathSpelling}`, async () => {
     expect(await rules.checkPathSpelling(path_fail_checkPathSpelling)).toBe(false)
 })
